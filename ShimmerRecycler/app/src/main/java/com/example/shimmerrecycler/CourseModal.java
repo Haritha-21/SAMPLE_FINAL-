66895f1757eae11309023604caf1527d3a799ef3
Haritha-21 <84864720+Haritha-21@users.noreply.github.com>
2021-07-09 14:23:05 +0530

    Add files via upload

---
+++ ShimmerRecycler/app/src/main/java/com/example/shimmerrecycler/CourseModal.java
@@ -0,0 +1,49 @@
+package com.example.shimmerrecycler;
+
+public class CourseModal {
+
+    private String courseName;
+    private String courseimg;
+    private String courseMode;
+    private String courseTracks;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseimg() {
+        return courseimg;
+    }
+
+    public void setCourseimg(String courseimg) {
+        this.courseimg = courseimg;
+    }
+
+    public String getCourseMode() {
+        return courseMode;
+    }
+
+    public void setCourseMode(String courseMode) {
+        this.courseMode = courseMode;
+    }
+
+    public String getCourseTracks() {
+        return courseTracks;
+    }
+
+    public void setCourseTracks(String courseTracks) {
+        this.courseTracks = courseTracks;
+    }
+
+    public CourseModal(String courseName, String courseimg, String courseMode, String courseTracks) {
+        this.courseName = courseName;
+        this.courseimg = courseimg;
+        this.courseMode = courseMode;
+        this.courseTracks = courseTracks;
+    }
+}
+
