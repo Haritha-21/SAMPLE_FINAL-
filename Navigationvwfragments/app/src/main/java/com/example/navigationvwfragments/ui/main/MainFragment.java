3134c002cf57d1ed14a7e3528a4337970d39b3dd
Haritha-21 <84864720+Haritha-21@users.noreply.github.com>
2021-08-01 18:14:01 +0530

    Add files via upload

---
+++ Navigationvwfragments/app/src/main/java/com/example/navigationvwfragments/ui/main/MainFragment.java
@@ -0,0 +1,51 @@
+package com.example.navigationvwfragments.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.navigationvwfragments.R;
+import com.example.navigationvwfragments.UserAccountsActivity;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        Button btnNext =view.findViewById(R.id.btn_nxt);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             startActivity(new Intent(getContext(), UserAccountsActivity.class));
+            }
+        });
+        return  view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
